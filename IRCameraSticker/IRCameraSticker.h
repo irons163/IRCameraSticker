132f3c5bb03ca710e82d91217b819bfa68472992
Your Name <you@example.com>
2021-02-25 16:33:14 +0800

    Initial Commit

---
+++ IRCameraSticker/IRCameraSticker.h
@@ -0,0 +1,18 @@
+//
+//  IRCameraSticker.h
+//  IRCameraSticker
+//
+//  Created by irons on 2021/2/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IRCameraSticker.
+FOUNDATION_EXPORT double IRCameraStickerVersionNumber;
+
+//! Project version string for IRCameraSticker.
+FOUNDATION_EXPORT const unsigned char IRCameraStickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IRCameraSticker/PublicHeader.h>
+
+
